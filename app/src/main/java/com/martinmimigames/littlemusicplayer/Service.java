8a78428afb15a4e3ff6e54ccfa31d89888df49d1
wmartinmimi <martinmimiw@gmail.com>
2022-08-25 15:54:12 +0100

    removed unnecessary import

--- app/src/main/java/com/martinmimigames/littlemusicplayer/Service.java
+++ app/src/main/java/com/martinmimigames/littlemusicplayer/Service.java
@@ -1,6 +1,5 @@
 package com.martinmimigames.littlemusicplayer;
 
-import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
