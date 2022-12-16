84aa4bba8e66d0780c3e763c4239ceab6b530c37
wmartinmimi <martinmimiw@gmail.com>
2022-12-16 20:20:08 +0000

    reduce code size

--- app/src/main/java/com/martinmimigames/littlemusicplayer/Notifications.java
+++ app/src/main/java/com/martinmimigames/littlemusicplayer/Notifications.java
@@ -57,63 +57,59 @@ public class Notifications {
   }
 
   /**
+   * Generate pending intents for service control
+   *
+   * @param id     the id for the intent
+   * @param action the control action
+   * @return the pending intent generated
+   */
+  PendingIntent genIntent(int id, byte action) {
+    /* flags for control logics on notification */
+    int pendingIntentFlag = PendingIntent.FLAG_IMMUTABLE;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE)
+      pendingIntentFlag |= PendingIntent.FLAG_UPDATE_CURRENT;
+
+    int intentFlag = Intent.FLAG_ACTIVITY_NO_HISTORY;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR)
+      intentFlag |= Intent.FLAG_ACTIVITY_NO_ANIMATION;
+
+    return PendingIntent
+      .getService(service, id, new Intent(service, Service.class)
+          .addFlags(intentFlag)
+          .putExtra(ServiceControl.TYPE, action)
+          .putExtra(ServiceControl.SELF_IDENTIFIER, ServiceControl.SELF_IDENTIFIER_ID)
+        , pendingIntentFlag);
+  }
+
+  /**
    * create and start playback control notification
    */
   void getNotification(final Uri uri) {
 
     /* setup notification variable */
     final String title = "now playing : " + new File(uri.getPath()).getName();
-    notification = NotificationHelper.createNotification(service, NOTIFICATION_CHANNEL, (Build.VERSION.SDK_INT > 21) ? android.app.Notification.CATEGORY_SERVICE : null);
+    notification = NotificationHelper.createNotification(service, NOTIFICATION_CHANNEL, (Build.VERSION.SDK_INT > 21) ? Notification.CATEGORY_SERVICE : null);
     notification.icon = R.drawable.ic_notif; // icon display
     //notification.largeIcon = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_launcher); // large icon display
-    notification.defaults = android.app.Notification.DEFAULT_ALL; // set defaults
+    notification.defaults = Notification.DEFAULT_ALL; // set defaults
     notification.when = System.currentTimeMillis(); // set time of notification
     notification.tickerText = title;// set popup text
-    notification.flags = android.app.Notification.FLAG_AUTO_CANCEL; // automatically close popup
-    notification.audioStreamType = android.app.Notification.STREAM_DEFAULT;
+    notification.flags = Notification.FLAG_AUTO_CANCEL; // automatically close popup
+    notification.audioStreamType = Notification.STREAM_DEFAULT;
     notification.sound = null;
 
-    /* flags for control logics on notification */
-    final int pendingIntentFlag =
-      (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) ?
-        (PendingIntent.FLAG_UPDATE_CURRENT |
-          ((Build.VERSION.SDK_INT > 23) ?
-            PendingIntent.FLAG_IMMUTABLE : 0)
-        ) : 0;
-    /* calls for control logic by starting activity with flags */
-    final PendingIntent killIntent =
-      PendingIntent
-        .getService(
-          service,
-          1,
-          new Intent(
-            service,
-            Service.class
-          ).addFlags(
-            Intent.FLAG_ACTIVITY_NO_HISTORY |
-              (
-                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) ?
-                  Intent.FLAG_ACTIVITY_NO_ANIMATION : 0)
-          ).putExtra(
-            ServiceControl.SELF_IDENTIFIER,
-            ServiceControl.SELF_IDENTIFIER_ID
-          ).putExtra(
-            ServiceControl.TYPE,
-            ServiceControl.KILL
-          ),
-          pendingIntentFlag
-        );
+    final PendingIntent killIntent = genIntent(1, ServiceControl.KILL);
 
     /* extra variables for notification setup */
     /* different depending on sdk version as they require different logic */
     if (Build.VERSION.SDK_INT >= 19) {
 
-      notification.extras.putCharSequence(android.app.Notification.EXTRA_TITLE, title);
-      notification.extras.putCharSequence(android.app.Notification.EXTRA_TEXT, "Tap to stop");
+      notification.extras.putCharSequence(Notification.EXTRA_TITLE, title);
+      notification.extras.putCharSequence(Notification.EXTRA_TEXT, "Tap to stop");
 
-      notification.contentIntent = PendingIntent.getService(service, 2, new Intent(service, Service.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION | Intent.FLAG_ACTIVITY_NO_HISTORY).putExtra(ServiceControl.TYPE, ServiceControl.PLAY_PAUSE).putExtra(ServiceControl.SELF_IDENTIFIER, ServiceControl.SELF_IDENTIFIER_ID), pendingIntentFlag);
+      notification.contentIntent = genIntent(2, ServiceControl.PLAY_PAUSE);
 
-      notification.actions = new android.app.Notification.Action[]{new android.app.Notification.Action(R.drawable.ic_launcher, "close", killIntent)};
+      notification.actions = new Notification.Action[]{new Notification.Action(R.drawable.ic_launcher, "close", killIntent)};
     } else {
       notification.contentView = new RemoteViews("com.martinmimigames.littlemusicplayer", R.layout.notif);
       NotificationHelper.setText(notification, R.id.notif_title, title);
@@ -121,7 +117,7 @@ public class Notifications {
     }
 
     /* set to not notify again when update */
-    notification.flags |= android.app.Notification.FLAG_ONLY_ALERT_ONCE;
+    notification.flags |= Notification.FLAG_ONLY_ALERT_ONCE;
     update();
 
   }
