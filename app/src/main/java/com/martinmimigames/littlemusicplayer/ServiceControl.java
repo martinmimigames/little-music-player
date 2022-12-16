9bf27077d7b77aff14f75a124c4e6b80fcc13b44
wmartinmimi <martinmimiw@gmail.com>
2022-12-16 20:36:34 +0000

    remove unused import

--- app/src/main/java/com/martinmimigames/littlemusicplayer/ServiceControl.java
+++ app/src/main/java/com/martinmimigames/littlemusicplayer/ServiceControl.java
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
 
 /**
