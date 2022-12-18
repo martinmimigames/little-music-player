31fb55a7a8433677be6a8eab54dcb3b8cd4aae45
wmartinmimi <martinmimiw@gmail.com>
2022-12-18 12:47:21 +0000

    better error report + http warning

--- app/src/main/java/com/martinmimigames/littlemusicplayer/Exceptions.java
+++ app/src/main/java/com/martinmimigames/littlemusicplayer/Exceptions.java
@@ -9,13 +9,13 @@ final class Exceptions {
   static final String IllegalState = "Unusable player state, close app and try again.";
   static final String IO = "Read error, try again later.";
   static final String Security = "File location protected, cannot be accessed.";
-
-  static final String MediaPlayerSystemError = "Media Player Error, maybe format not support by system.";
+  static final String FormatNotSupported = "Media Player Error, maybe format not support";
+  static final String UsingHttp = "Using http for streaming, may be insecure.";
 
   /**
    * create and display error toast to report errors
    */
   static void throwError(Context context, String msg) {
-    ToastHelper.showShort(context, msg);
+    ToastHelper.showLong(context, msg);
   }
 }
