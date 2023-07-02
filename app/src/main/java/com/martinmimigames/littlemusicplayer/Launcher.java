e225f10e35d628ec93007301f4fa70cfecfb2d88
wmartinmimi <martinmimiw@gmail.com>
2023-07-02 20:36:30 +0100

    update images

--- app/src/main/java/com/martinmimigames/littlemusicplayer/Launcher.java
+++ app/src/main/java/com/martinmimigames/littlemusicplayer/Launcher.java
@@ -1,9 +1,7 @@
 package com.martinmimigames.littlemusicplayer;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
