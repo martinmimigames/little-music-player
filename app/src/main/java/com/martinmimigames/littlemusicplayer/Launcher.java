2aa34d45a31642112dccad697504da531853d76b
wmartinmimi <martinmimiw@gmail.com>
2023-07-19 19:37:50 +0100

    added folder as playlist for android 5.0+

--- app/src/main/java/com/martinmimigames/littlemusicplayer/Launcher.java
+++ app/src/main/java/com/martinmimigames/littlemusicplayer/Launcher.java
@@ -1,12 +1,16 @@
 package com.martinmimigames.littlemusicplayer;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.provider.Settings;
 import android.view.View;
+
+import java.util.ArrayList;
 
 /**
  * activity for controlling the playback by invoking different logics based on incoming intents
@@ -21,7 +25,14 @@ public class Launcher extends Activity {
     public static final byte PAUSE = 4;
     public static final byte LOOP = 5;
     public static final byte SKIP = 6;
-    public static final int REQUEST_CODE = 44130840;
+    private static final int FILE_REQUEST_CODE = 44130840;
+    private static final int FOLDER_REQUEST_CODE = 44130841;
+
+    @SuppressLint("InlinedApi")
+    private static final String[] FOLDER_PROJECTION = {
+            DocumentsContract.Document.COLUMN_DOCUMENT_ID,
+            DocumentsContract.Document.COLUMN_MIME_TYPE
+    };
 
     /**
      * redirect call to actual logic
@@ -35,14 +46,23 @@ public class Launcher extends Activity {
                 && !Intent.ACTION_SEND_MULTIPLE.equals(getIntent().getAction())) {
 
             /* set listener for button */
-            findViewById(R.id.file_opener)
-                    .setOnClickListener(
-                            v -> {
-                                /* request a file from the system */
-                                final Intent fileIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                                fileIntent.setType("audio/*");
-                                startActivityForResult(fileIntent, Launcher.REQUEST_CODE);
-                            });
+            findViewById(R.id.file_opener).setOnClickListener(v -> {
+                /* request a file from the system */
+                final Intent fileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                fileIntent.setType("audio/*");
+                startActivityForResult(fileIntent, Launcher.FILE_REQUEST_CODE);
+            });
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                findViewById(R.id.folder_opener).setVisibility(View.VISIBLE);
+                /* set listener for button */
+                findViewById(R.id.folder_opener).setOnClickListener(v -> {
+                    /* request a file from the system */
+                    final Intent fileIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+                    startActivityForResult(fileIntent, Launcher.FOLDER_REQUEST_CODE);
+                });
+            }
+
             return;
         }
         onIntent(getIntent());
@@ -102,10 +122,26 @@ public class Launcher extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         /* if result unusable, discard */
-        if (requestCode != Launcher.REQUEST_CODE || resultCode != Activity.RESULT_OK) return;
-        /* redirect to service */
-        intent.setClass(this, Launcher.class);
-        intent.setAction(Intent.ACTION_VIEW);
-        startActivity(intent);
+        if (resultCode != Activity.RESULT_OK) return;
+        if (requestCode == Launcher.FILE_REQUEST_CODE) {/* redirect to service */
+            intent.setClass(this, Launcher.class);
+            intent.setAction(Intent.ACTION_VIEW);
+            startActivity(intent);
+        } else if (requestCode == Launcher.FOLDER_REQUEST_CODE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ArrayList<Uri> audioList;
+            try (var cursor = getContentResolver().query(DocumentsContract.buildChildDocumentsUriUsingTree(intent.getData(), DocumentsContract.getTreeDocumentId(intent.getData())), FOLDER_PROJECTION, null, null, null)) {
+                audioList = new ArrayList<>(cursor.getCount());
+                while (cursor.moveToNext()) {
+                    var mimeType = cursor.getString(1);
+                    if (mimeType.startsWith("audio/")) {
+                        audioList.add(DocumentsContract.buildDocumentUriUsingTree(intent.getData(), cursor.getString(0)));
+                    }
+                }
+            }
+            intent.setClass(this, Launcher.class);
+            intent.setAction(Intent.ACTION_SEND_MULTIPLE);
+            intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, audioList);
+            startActivity(intent);
+        }
     }
 }
