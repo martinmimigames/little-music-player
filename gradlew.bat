0b3184217bd62fb561d8069450421e28b552aa8b
b39070566 <77479357+b39070566@users.noreply.github.com>
2024-01-16 10:41:15 +0800

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -14,7 +14,7 @@
 @rem limitations under the License.
 @rem
 
-@if "%DEBUG%" == "" @echo off
+@if "%DEBUG%" == "" 
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
